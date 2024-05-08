472c7b90221231c739ce0ab5c0f877395ae23246
Rahul KJ <kjrahu77@gmail.com>
2024-05-09 04:23:21 +0530

    commit

--- page.c
+++ page.c
@@ -20,58 +20,6 @@ void fifo(int a[],int n,int frames)
     }
     printf("Page faults = %d\n",miss);
 }
-void lru(int a[],int n,int frames)
-{
-    int miss=0,c=0,loc,min,i,j;
-    for(i=0;i<n;i++){
-        time[a[i]]=i;
-        if(inframe[a[i]]==0){
-            if(c<frames)
-            	q[c++]=a[i];
-            else
-            {
-            	     for(j=1,loc=0,min=time[q[0]];j<frames;j++){
-            	            if((time[q[j]])<min)
-            	            {
-            	            	min=time[q[j]];
-            	            	loc=j;
-            	            }
-            	     }
-            	     inframe[q[loc]]=0;
-            	     q[loc]=a[i];
-            }
-            inframe[a[i]]=1;            
-            miss++;
-        }
-    }
-    printf("Page faults = %d\n",miss);
-}
-void lfu(int a[],int n,int frames)
-{
-    int miss=0,c=0,loc,min,i,j;
-    for(i=0;i<n;i++){
-        count[a[i]]++;
-        if(inframe[a[i]]==0){
-            if(c<frames)
-            	q[c++]=a[i];
-            else
-            {
-            	     for(j=1,loc=0,min=count[q[0]];j<frames;j++){
-            	            if((count[q[j]])<min)
-            	            {
-            	            	min=count[q[j]];
-            	            	loc=j;
-            	            }
-            	     }
-            	     inframe[q[loc]]=0;
-            	     q[loc]=a[i];
-            }
-            inframe[a[i]]=1;            
-            miss++;
-        }
-    }
-    printf("Page faults = %d\n",miss);
-}
 void main() {
 	int i,n,a[MAX],frames,ch;
 	printf("Enter the number of references in reference string :");
@@ -80,24 +28,12 @@ void main() {
 		scanf("%d",&a[i]);
 	printf("Enter the number of frames :");
 	scanf("%d",&frames);
-    do{
-        for(i=0;i<n;i++)
-        {   
-            count[a[i]]=0;
-            time[a[i]]=0;
-            inframe[a[i]]=0;
-        }
-        printf("Enter 1:FIFO 2:LRU 3:LFU 4:Exit :\n");
-        scanf("%d",&ch);
-
-        switch(ch){
-            case 1:fifo(a,n,frames);
-                    break;
-            case 2:lru(a,n,frames);
-                    break;
-            case 3:lfu(a,n,frames);
-            case 4:break;
-            default:printf("Invalid input\n");
-        }
-    }while(ch!=4);
+    for(i=0;i<n;i++)
+    {   
+        count[a[i]]=0;
+        time[a[i]]=0;
+        inframe[a[i]]=0;
+    }
+    printf("FIFO \n");
+    fifo(a,n,frames);
 }
